6bdac4ae72d8330f245de51050267fc40d3823e0
30Piraten <victorehikioyaraeva@outlook.com>
2025-01-19 17:38:01 +0100

    Updated app logic

--- frontend/handlers/app.go
+++ frontend/handlers/app.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/template/html/v2"
 )
 
 // PhotoOrder represents the structure of our form data
@@ -17,13 +16,13 @@ type PhotoOrder struct {
 }
 
 // SetupRoutes configures the routes for our photo upload service
-func SetupRoutes(app *fiber.App) {
-	// Initialize template engine
-	engine := html.New("./views", ".html")
+func Handler(app *fiber.App) {
+	// // Initialize template engine
+	// engine := html.New("./views", ".html")
 
-	app = fiber.New(fiber.Config{
-		Views: engine,
-	})
+	// app = fiber.New(fiber.Config{
+	// 	Views: engine,
+	// })
 
 	// Render the upload form
 	app.Get("/", func(c *fiber.Ctx) error {
@@ -56,6 +55,7 @@ func SetupRoutes(app *fiber.App) {
 		// Process each uploaded file
 		for _, file := range files {
 			// Save file to disk or cloud storage
+			// logic to store photos in S3 bucket!
 			err := c.SaveFile(file, fmt.Sprintf("./uploads/%s", file.Filename))
 			if err != nil {
 				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
