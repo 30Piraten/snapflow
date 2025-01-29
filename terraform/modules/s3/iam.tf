aa68cf38c3e3d5d9f9358d31ccf1ccd2babce81c
30Piraten <victorehikioyaraeva@outlook.com>
2025-01-29 12:50:33 +0100

    Added S3 policies for CloudFront

--- terraform/modules/s3/iam.tf
+++ terraform/modules/s3/iam.tf
@@ -41,3 +41,48 @@ resource "aws_iam_role_policy" "iam_role_policy" {
     })
   
 }
+
+# IAM policy for CloudFront to access S3 bucket
+resource "aws_s3_bucket_policy" "snapflow_s3_policy" {
+    bucket = aws_s3_bucket.processed_image_bucket.id
+
+    policy = jsonencode({
+        Version = "2012-10-17",
+        Statement = [
+            {
+                Sid = "AllowCloudFrontReadAccess"
+                Effect = "Allow"
+                Principal = {
+                    AWS = aws_cloudfront_origin_access_identity.snapflow_origin_access_identity.iam_arn
+                }
+                Action = "s3:GetObject"
+                Resource = "${aws_s3_bucket.processed_image_bucket.arn}/*"
+            }
+        ]
+    })
+}
+
+// Logging policy for CloudFront logs
+resource "aws_s3_bucket_policy" "logging_bucket_policy" {
+    bucket = aws_s3_bucket.logging_bucket.id
+
+    policy = jsonencode({
+        Version = "2012-10-17",
+        Statement = [
+            {
+                Sid = "AllowCloudFrontLogging"
+                Effect = "Allow"
+                Principal = {
+                    Service = "cloudfront.amazonaws.com"
+                }
+                Action = "s3:PutObject"
+                Resource = "${aws_s3_bucket.logging_bucket.arn}/*"
+                Condition = {
+                    StringEquals = {
+                        "AWS:SourceArn" = aws_cloudfront_distribution.snapflow_cloudfront.arn
+                    }
+                }
+            }
+        ]
+    })
+}
