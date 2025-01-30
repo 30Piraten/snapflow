89c04b6814af9003b2c406852064dc6255d50246
30Piraten <victorehikioyaraeva@outlook.com>
2025-01-30 05:56:31 +0100

    Update

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,11 @@
 output "s3_bucket_name" {
   value = module.s3.s3_bucket_name
 }
+
+output "dynamodb_table_name" {
+  value = module.dynamodb.dynamodb_table_name
+}
+
+output "sns_topic_arn" {
+  value = module.notification.sns_topic_arn
+}
