d6002e49699eb72586b9c0ebee73964be894dd2a
30Piraten <victorehikioyaraeva@outlook.com>
2025-02-01 07:37:44 +0100

    Logic modified

--- frontend/script/script.sh
+++ frontend/script/script.sh
@@ -1,21 +1,18 @@
-#!/bin/bash
+# #!/bin/bash
 
 # Navigate to the directory containing lambda.go (Important!)
 cd ../lambda
 
+# Clean up
+rm -f bootstrap dummyprinter.zip
+
 # Compile Lambda function and name the output "bootstrap"
-GOOS=linux GOARCH=amd64 go build -o bootstrap lambda.go
-
-# Create the zip file including the "bootstrap" binary
-zip dummyprinter.zip bootstrap
-
-# Verify the zip file contents
-zipinfo -e dummyprinter.zip
+GOOS=linux GOARCH=amd64 go CGO_ENABLED=0 build -o bootstrap lambda.go
 
 # Make "bootstrap" executable (Important!)
 chmod +x bootstrap
 
-# Verify the zip file contents and permissions (crucial!)
-# zipinfo dummyprinter.zip
+# Create the zip file including the "bootstrap" binary
+zip -X dummyprinter.zip bootstrap
 
 echo "Lambda deployment package (dummyprinter.zip) created."
