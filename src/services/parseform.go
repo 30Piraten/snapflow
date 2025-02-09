6bb83153d37b78503d0ab56d335a0a247e8fe96c
30Piraten <victorehikioyaraeva@outlook.com>
2025-02-09 11:42:59 +0100

    Updated file logic to c.FormValue instead of BodyParser

--- src/services/parseform.go
+++ src/services/parseform.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"fmt"
+
 	"github.com/30Piraten/snapflow/models"
 	"github.com/30Piraten/snapflow/utils"
 	"github.com/gofiber/fiber/v2"
@@ -12,19 +14,40 @@ import (
 func ParseOrderDetails(c *fiber.Ctx) (*models.PhotoOrder, error) {
 	order := new(models.PhotoOrder)
 
-	if err := c.BodyParser(order); err != nil {
-		utils.Logger.Error("Form parsing failed", zap.Error(err))
+	// Parse the multipart form
+	form, err := c.MultipartForm()
+	if err != nil {
+		utils.Logger.Error("Multipart form parsing failed", zap.Error(err))
 		return nil, err
 	}
 
-	// Validate required fields
+	// Get form values
+	order.FullName = c.FormValue("fullName")
+	order.Location = c.FormValue("location")
+	order.Size = c.FormValue("size")
+	order.PaperType = c.FormValue("paperType")
+	order.Email = c.FormValue("email")
+
+	// Get files from form
+	files := form.File["photos"]
+	if len(files) == 0 {
+		utils.Logger.Error("No photos found in the form data")
+		return nil, fmt.Errorf("no photos provided in the order")
+	}
+
+	// Assign files to order
+	order.Photos = files
+
+	// Log the number of files received
+	utils.Logger.Info("Files received",
+		zap.Int("fileCount", len(files)),
+		zap.String("fullName", order.FullName),
+		zap.String("email", order.Email))
+
+	// Validate the required fields
 	if err := ValidateOrder(order); err != nil {
 		return nil, err
 	}
-
-	utils.Logger.Info("Order details parsed successfully",
-		zap.String("fullName", order.FullName),
-		zap.String("email", order.Email))
 
 	return order, nil
 }
