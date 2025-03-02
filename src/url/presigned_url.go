19b2a4b33c4bb312c26c02ea9733ea2830ef1f6b
REV <victorehikioyaraeva@outlook.com>
2025-03-02 05:10:03 +0200

    Update presigned_url.go

--- src/url/presigned_url.go
+++ src/url/presigned_url.go
@@ -115,7 +115,8 @@ func GeneratePresignedURL(order *models.PhotoOrder) (*PresignedURLResponse, erro
 	}
 
 	// Removed SNS Notification for initial confirmation. Since
-	// we only need to send it once, not twice.
+	// We only need to send it once, after the print has been completed.
+	// Not before print and after print - customers wait in-store to collect.
 	// // Send notification via SNS after the print job completes
 	// err = cfg.SendSNSNotification(orderID, order.Email)
 	// if err != nil {
